ec5076abb4de1bd3ab7df1e5dfa12c747d7748e8
First <ritikisnamaheshwari2002@gmail.com>
2023-11-18 10:55:48 +0530

    practical hai

---
+++ calci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,d;
+    printf("Your Calculator\n1. Add\n2. Mul\n3. Divide\n Enter the values of a,b,n respectively\n");
+    scanf("%d %d %d",&a,&b,&n);
+    switch(n)
+    {
+        case 1: printf("\na-b = %d",a-b);break;
+        case 2: printf("\na*b = %d",a*b);break;
+        case 3: printf("\na/b = %d",a/b);break;
+    }
+    return 0;
+}
